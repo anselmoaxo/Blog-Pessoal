299817836b830383ddfdd5244ade7c6213bd642d
Anselmo Xavier <anselmo.cstecnologia@gmail.com>
2023-12-11 23:56:21 -0300

    criando model MenuLink e registrando no admin

--- site_setup/admin.py
+++ site_setup/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import MenuLink
 
-# Register your models here.
+@admin.register(MenuLink)
+class MenuLinkAdmin(admin.ModelAdmin):
+    list_display = 'id', 'text', 'url_or_path',
+    search_fields = 'id', 'text', 'url_or_path',
+    list_display_links = 'id', 'text', 'url_or_path',
+    
+    
+
+
