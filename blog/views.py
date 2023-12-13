3e1b76974486258a54c35bdbe33dec539869caaf
Anselmo Xavier <anselmo.cstecnologia@gmail.com>
2023-12-13 00:22:15 -0300

    add pages post e page

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'blog/pages/index.html')
 
+def post(request):
+    return render(request, 'blog/pages/post.html')
+
+def page(request):
+    return render(request, 'blog/pages/page.html')
+
